2c4c69c659c1493d5fc593d6f5b39383a70e782b
Apppp11 <drsbp51@gmail.com>
2022-10-04 14:44:06 +0900

    構文木の作成・四則演算の追加

--- 0cc.c
+++ 0cc.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <string.h>
 
+//トークン列用構造体
 typedef enum
 {
     TK_NUM,
@@ -21,9 +22,30 @@ struct Token
     char *str;
 };
 
+//抽象構文木用構造体
+typedef enum
+{
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+struct Node
+{
+    NodeKind kind;
+    Node *l_child;
+    Node *r_child;
+    int val;
+};
+
+//変数宣言
 Token *token;
 char *user_input;
 
+//エラー出力
 void error_at(char *loc, char *fmt, ...)
 {
     va_list ap;
@@ -38,6 +60,7 @@ void error_at(char *loc, char *fmt, ...)
     exit(1);
 }
 
+//トークン列生成
 bool consume_operator(char op)
 {
     if (token->kind != TK_OPERATOR || token->str[0] != op)
@@ -89,7 +112,7 @@ Token *tokenize(char *p)
             continue;
         }
 
-        if (*p == '+' || *p == '-')
+        if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')')
         {
             cur_token = create_token(TK_OPERATOR, cur_token, p++);
             continue;
@@ -106,6 +129,115 @@ Token *tokenize(char *p)
     create_token(TK_EOF, cur_token, p);
     return head.next_token;
 }
+
+//構文木生成
+Node *create_op_node(NodeKind kind, Node *l_child, Node *r_child);
+Node *create_num_node(int val);
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *create_op_node(NodeKind kind, Node *l_child, Node *r_child)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->l_child = l_child;
+    node->r_child = r_child;
+    return node;
+}
+
+Node *create_num_node(int val)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr()
+{
+    Node *node = mul();
+    while (1)
+    {
+
+        if (consume_operator('+'))
+        {
+            node = create_op_node(ND_ADD, node, mul());
+        }
+        else if (consume_operator('-'))
+        {
+            node = create_op_node(ND_SUB, node, mul());
+        }
+        else
+        {
+            return node;
+        }
+    }
+}
+Node *mul()
+{
+    Node *node = primary();
+    while (1)
+    {
+        if (consume_operator('*'))
+        {
+            node = create_op_node(ND_MUL, node, primary());
+        }
+        else if (consume_operator('/'))
+        {
+            node = create_op_node(ND_DIV, node, primary());
+        }
+        else
+        {
+            return node;
+        }
+    }
+}
+Node *primary()
+{
+    if (consume_operator('('))
+    {
+        Node *node = expr();
+        expect_operator(')');
+        return node;
+    }
+    return create_num_node(expect_number());
+}
+
+//アセンブリ生成
+void gen(Node *node)
+{
+    if (node == NULL)
+        exit(1);
+    if (node->kind == ND_NUM)
+    {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    //演算子の場合
+    gen(node->l_child);
+    gen(node->r_child);
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+    switch (node->kind)
+    {
+    case ND_ADD:
+        printf("    add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("    sub rax,rdi\n");
+        break;
+    case ND_MUL:
+        printf("    imul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("    cqo\n");
+        printf("    idiv rdi\n");
+        break;
+    }
+    printf("    push rax\n");
+}
 int main(int argc, char **argv)
 {
     if (argc != 2)
@@ -113,25 +245,17 @@ int main(int argc, char **argv)
         printf("引数の個数が正しくないです\n");
         return 1;
     }
+    //トークン列作成
     user_input = argv[1];
     token = tokenize(argv[1]);
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("    mov rax, %d\n", expect_number());
 
-    while (!at_eof())
-    {
-        if (consume_operator('+'))
-        {
-            printf("    add rax, %d\n", expect_number());
-        }
-        else
-        {
-            expect_operator('-');
-            printf("    sub rax, %d\n", expect_number());
-        }
-    }
+    //構文木作成
+    Node *node = expr();
+    gen(node);
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
--- test.sh
+++ test.sh
@@ -9,15 +9,21 @@ assert(){
     if [ "$actual" = "$expected" ]; then
         echo "$input => $actual"
     else
-        echo "$input => $ecpected expected, but got $actual"
+        echo "$input => $expected expected, but got $actual"
         exit 1
     fi
 }
-
+#整数を返す
 assert 0 0
 assert 1 1
 assert 32 32
+#足し算引き算
 assert 21 "5+20-4"
+#スペースの除去
 assert 41 " 12 + 34 - 5 "
+#四則演算・括弧
+assert 47 '5+6*7'
+assert 15 '5*(9-6)'
+assert 4 '(3+5)/2'
 
 echo OK
